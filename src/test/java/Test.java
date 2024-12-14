b1764e2db048b061de3699e8451de3c128c2903e
unknown <sunnykumar123.sk98@gmail.com>
2024-12-14 23:53:03 +0530

    Added Get user method

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,21 +1,35 @@
 import Pojos.UserDTO;
 import io.restassured.RestAssured;
+import net.bytebuddy.build.Plugin;
+import net.bytebuddy.implementation.bind.annotation.BindingPriority;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 
 public class Test {
+    UserDTO userdto;
     @BeforeClass
     public void setup()
     {
         RestAssured.baseURI="https://petstore.swagger.io/v2";
 
     }
-    @org.testng.annotations.Test
+    @org.testng.annotations.Test(priority = 0)
     public void createUserByUserName()
     {
-        UserDTO userdto=UserDTO.builder().id(1).username("Subbu").firstName("M").lastName("Subramanyam")
+         userdto=UserDTO.builder().id(1).username("Subbu").firstName("M").lastName("Subramanyam")
                 .email("msubbu@3208").password("Hekl123").phone("8178474534").userStatus(1).build();
         RestAssured.given().log().all().header("Content-Type","application/json").body(userdto)
                 .when().post("/user").then().log().all().statusCode(200);
         userdto.setUsername("Subbu");
+    }
+    @org.testng.annotations.Test(priority = 1)
+
+    public void getUserByUser()
+    {
+
+        String ActualUsrerName = RestAssured.given().log().all().pathParam("username", userdto.getUsername()).when().get("/user/{username}/")
+                .then().log().all().statusCode(200).extract().response().jsonPath().get("username").toString();
+        Assert.assertEquals(ActualUsrerName,"Sunny","User name are not equal");
+
     }
 }
